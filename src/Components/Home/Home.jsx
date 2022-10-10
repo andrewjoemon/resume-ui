c146a54a6ffab20a8f93356b005673fe7f49c3db
andrewjoemon <andrewjoemon10@gmail.com>
2022-10-10 13:02:35 +0530

    hash router added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import './Home.css';
-import {Link} from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter, Link} from "react-router-dom";
 
 function Home({Reset}){
    
     return(
         <div className="r4">
         <h1 className="hclass">WELCOME TO MY PAGE</h1>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
         <h1><Link to='/resume-ui/Form' onFocus={Reset} id="centerbtn">Create New Resume</Link></h1>
-        </BrowserRouter>
+        </HashRouter>
         </div>
     );
     }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './navbar.css';
-import { Link } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter, Link } from "react-router-dom";
 
 function Nav({Reset}){ 
     const Load=()=>{
@@ -12,12 +11,12 @@ function Nav({Reset}){
        
         
         <header className="t46" >
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <HashRouter basename={process.env.PUBLIC_URL}>
 
             <Link to="/resume-ui" id="t47">Home</Link>
             <Link to="/resume-ui/Temp"  onClick={Load} id="t47">Templates</Link>
             <Link to="/resume-ui/About" id="t47">About</Link>
-            </BrowserRouter>
+            </HashRouter>
             
             
         </header>
